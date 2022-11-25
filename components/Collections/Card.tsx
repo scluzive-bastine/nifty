a07a55ea7fdba55095893b71a8eb191ffebb7db7
Sabastine <toshfuture@gmail.com>
2022-11-25 22:58:45 +0100

    Collections mobile responsiveness added

--- components/Collections/Card.tsx
+++ components/Collections/Card.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
 
 import { RiShieldCheckFill } from 'react-icons/ri'
 
@@ -10,32 +11,34 @@ interface Props {
 
 const Card = ({ width, image, showMore }: Props) => {
   return (
-    <div className={`bg-darkGray p-3 border border-gray-20 rounded-xl ${width} flex-shrink-0`}>
-      <div className='w-full relative'>
-        <Image src={image} className='w-full h-full object-cover rounded-xl' alt='Hape' />
-      </div>
-      <article className='mt-3'>
-        <div className='flex space-x-2 items-center'>
-          <h2 className={`text-white ${showMore ? 'text-sm' : 'text-lg '} font-semibold`}>
-            NFT Name
-          </h2>
-          <RiShieldCheckFill className='text-blue-400' />
+    <Link href={`/token/nft`}>
+      <div className={`bg-darkGray p-3 border border-gray-20 rounded-xl ${width} flex-shrink-0`}>
+        <div className='w-full relative'>
+          <Image src={image} className='w-full h-full object-cover rounded-xl' alt='Hape' />
         </div>
-        {showMore && <h3 className='text-xs text-gray-400'>Price: 0.223 ETH</h3>}
-        {!showMore ? (
-          <div className='flex space-x-3 md:space-x-5 mt-5'>
-            <div>
-              <p className='text-gray-400 uppercase text-[6px] md:text-[10px]'>Day Volume</p>
-              <h3 className='text-gray-200 font-semibold text-sm lg:text-lg'>234 ETH</h3>
-            </div>
-            <div>
-              <p className='text-gray-400 uppercase text-[6px] md:text-[10px]'>Floor Price</p>
-              <h3 className='text-gray-200 font-semibold text-sm lg:text-lg'>1.2 ETH</h3>
-            </div>
+        <article className='mt-3'>
+          <div className='flex space-x-2 items-center'>
+            <h2 className={`text-white ${showMore ? 'text-sm' : 'text-lg '} font-semibold`}>
+              NFT Name
+            </h2>
+            <RiShieldCheckFill className='text-blue-400' />
           </div>
-        ) : null}
-      </article>
-    </div>
+          {showMore && <h3 className='text-xs text-gray-400'>Price: 0.223 ETH</h3>}
+          {!showMore ? (
+            <div className='flex space-x-3 md:space-x-5 mt-5'>
+              <div>
+                <p className='text-gray-400 uppercase text-[6px] md:text-[10px]'>Day Volume</p>
+                <h3 className='text-gray-200 font-semibold text-sm lg:text-lg'>234 ETH</h3>
+              </div>
+              <div>
+                <p className='text-gray-400 uppercase text-[6px] md:text-[10px]'>Floor Price</p>
+                <h3 className='text-gray-200 font-semibold text-sm lg:text-lg'>1.2 ETH</h3>
+              </div>
+            </div>
+          ) : null}
+        </article>
+      </div>
+    </Link>
   )
 }
 
--- pages/collections.tsx
+++ pages/collections.tsx
@@ -17,7 +17,7 @@ const collections = () => {
       <div className='mx-auto max-w-screen-lg 2xl:max-w-screen-xl mt-20 px-5'>
         <div className='grid grid-cols-1 md:grid-cols-5 gap-5'>
           <h2 className='text-gray-400 uppercase text-xs col-auto md:col-span-2'>Collections</h2>
-          <div className='grid grid-cols-6 gap-2 col-auto md:col-span-3'>
+          <div className='hidden lg:grid grid-cols-6 gap-2 col-auto md:col-span-3'>
             <div className='text-gray-400 uppercase text-xs text-right'>Volume</div>
             <div className='text-gray-400 uppercase text-xs text-right'>Floor Price</div>
             <div className='text-gray-400 uppercase text-xs text-right'>% Change</div>
@@ -27,41 +27,97 @@ const collections = () => {
           </div>
         </div>
       </div>
-      <section className='bg-black-50 p-3 rounded-xl mx-auto max-w-screen-lg 2xl:max-w-screen-xl mt-5'>
-        <Link href='/collection/nft'>
-          <div className='grid grid-cols-1 md:grid-cols-5 gap-5 items-center mb-5 hover:bg-darkGray px-2 py-2 md:px-5 md:py-2 rounded-xl transition duration-200'>
-            <div className='col-auto md:col-span-2'>
-              <div className='flex space-x-2 items-center'>
-                <h2 className='text-white text-lg font-semibold'>NFT Collection</h2>
-                <RiShieldCheckFill className='text-blue-400' />
-              </div>
-              <div className='flex space-x-5 md:space-x-0 touch-pan-x snap-x snap-mandatory snap-center overflow-x-scroll scroll-smooth scrollbar-hide lg:grid lg:grid-cols-6 md:gap-2 mt-2'>
-                {[1, 2, 3, 4, 5, 6].map((index) => (
-                  <div
-                    className='rounded-lg relative flex-shrink-0 max-w-[100px] max-h-[100px]'
-                    key={index}
-                  >
-                    <Image
-                      src={holder}
-                      className='w-full h-full object-cover rounded-lg'
-                      alt='Hape'
-                    />
+      <section className='p-4 mx-auto max-w-screen-lg 2xl:max-w-screen-xl mt-5 border border-gray-20 rounded-xl'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-1 gap-2'>
+          <Link href='/collection/nft'>
+            <div className='grid grid-cols-1 xl:grid-cols-5 gap-5 items-center mb-2 hover:bg-black-50/50 px-2 py-2 md:px-5 md:py-5 rounded-xl transition duration-200'>
+              <div className='col-auto md:col-span-2'>
+                <div className='flex space-x-2 items-center'>
+                  <h2 className='text-white text-lg font-semibold'>NFT Collection</h2>
+                  <RiShieldCheckFill className='text-blue-400' />
+                </div>
+                <div className='flex lg:hidden w-full mt-4'>
+                  <div className='w-full'>
+                    <h4 className='text-gray-400 text-xs'>Day Volume</h4>
+                    <h4 className='text-white text-xl'>56.5 ETH</h4>
                   </div>
-                ))}
+                  <div className='w-full'>
+                    <h4 className='text-gray-400 text-xs'>Floor Price</h4>
+                    <h4 className='text-white text-xl'>0.9 ETH</h4>
+                  </div>
+                </div>
+                <div className='flex space-x-5 md:space-x-0 touch-pan-x snap-x snap-mandatory snap-center overflow-x-scroll scroll-smooth scrollbar-hide lg:grid lg:grid-cols-6 md:gap-2 mt-2'>
+                  {[1, 2, 3, 4, 5, 6].map((index) => (
+                    <div
+                      className='rounded-lg relative flex-shrink-0 max-w-[100px] max-h-[100px]'
+                      key={index}
+                    >
+                      <Image
+                        src={holder}
+                        className='w-full h-full object-cover rounded-lg'
+                        alt='Hape'
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className='col-auto md:col-span-3 hidden lg:block'>
+                <div className='grid grid-cols-6 gap-2'>
+                  <div className='text-gray-200 uppercase text-lg text-right'>64 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>0.2 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>+12%</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>20 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>50 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>7,120</div>
+                </div>
               </div>
             </div>
-            <article className='col-auto md:col-span-3'>
-              <div className='grid grid-cols-6 gap-2'>
-                <div className='text-gray-200 uppercase text-lg text-right'>64 ETH</div>
-                <div className='text-gray-200 uppercase text-lg text-right'>0.2 ETH</div>
-                <div className='text-gray-200 uppercase text-lg text-right'>+12%</div>
-                <div className='text-gray-200 uppercase text-lg text-right'>20 ETH</div>
-                <div className='text-gray-200 uppercase text-lg text-right'>50 ETH</div>
-                <div className='text-gray-200 uppercase text-lg text-right'>7,120</div>
+          </Link>
+          <Link href='/collection/nft'>
+            <div className='grid grid-cols-1 xl:grid-cols-5 gap-5 items-center mb-2 hover:bg-black-50/50 px-2 py-2 md:px-5 md:py-5 rounded-xl transition duration-200'>
+              <div className='col-auto md:col-span-2'>
+                <div className='flex space-x-2 items-center'>
+                  <h2 className='text-white text-lg font-semibold'>NFT Collection</h2>
+                  <RiShieldCheckFill className='text-blue-400' />
+                </div>
+                <div className='flex lg:hidden w-full mt-4'>
+                  <div className='w-full'>
+                    <h4 className='text-gray-400 text-xs'>Day Volume</h4>
+                    <h4 className='text-white text-xl'>56.5 ETH</h4>
+                  </div>
+                  <div className='w-full'>
+                    <h4 className='text-gray-400 text-xs'>Floor Price</h4>
+                    <h4 className='text-white text-xl'>0.9 ETH</h4>
+                  </div>
+                </div>
+                <div className='flex space-x-5 md:space-x-0 touch-pan-x snap-x snap-mandatory snap-center overflow-x-scroll scroll-smooth scrollbar-hide lg:grid lg:grid-cols-6 md:gap-2 mt-2'>
+                  {[1, 2, 3, 4, 5, 6].map((index) => (
+                    <div
+                      className='rounded-lg relative flex-shrink-0 max-w-[100px] max-h-[100px]'
+                      key={index}
+                    >
+                      <Image
+                        src={holder}
+                        className='w-full h-full object-cover rounded-lg'
+                        alt='Hape'
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
-            </article>
-          </div>
-        </Link>
+              <div className='col-auto md:col-span-3 hidden lg:block'>
+                <div className='grid grid-cols-6 gap-2'>
+                  <div className='text-gray-200 uppercase text-lg text-right'>64 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>0.2 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>+12%</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>20 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>50 ETH</div>
+                  <div className='text-gray-200 uppercase text-lg text-right'>7,120</div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
       </section>
     </>
   )
